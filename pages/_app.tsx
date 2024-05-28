e2b75999bd74ca1b5912e6a2c193d4f622cc8bb8
dinklang <sdinklang@gmail.com>
2024-05-28 15:54:58 -0500

    coming soon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { Fragment } from "react";
-import Head from "next/head";
-import type { AppProps } from "next/app";
-import "./global.css";
-import "antd/dist/antd.css";
+import { Fragment } from "react"
+import Head from "next/head"
+import type { AppProps } from "next/app"
+import "./global.css"
+import "antd/dist/antd.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
-        <title>cilanding</title>
+        <title>Connect Internal</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <Component {...pageProps} />
     </Fragment>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,32 @@
-import type { NextPage } from "next";
-import Header from "../components/header";
-import HeroSection from "../components/hero-section";
-import AreasContainer from "../components/areas-container";
-import ContactContainer from "../components/contact-container";
-import ConnectForm from "../components/connect-form";
+import type { NextPage } from "next"
+import Header from "../components/header"
+import HeroSection from "../components/hero-section"
+import AreasContainer from "../components/areas-container"
+import ContactContainer from "../components/contact-container"
+import ConnectForm from "../components/connect-form"
+import Image from "next/image"
 
 const LandingPage: NextPage = () => {
   return (
-    <div className="relative bg-gray-white w-full flex flex-col items-center justify-start">
-      <Header />
-      <HeroSection />
-      <AreasContainer />
-      <ContactContainer />
-      <ConnectForm />
+    // <div className="relative bg-gray-white w-full flex flex-col items-center justify-start">
+    //   <Header />
+    //   <HeroSection />
+    //   <AreasContainer />
+    //   <ContactContainer />
+    //   <ConnectForm />
+    // </div>
+    <div className="flex flex-col items-center justify-center min-h-[100dvh] bg-[#d1f7c4] dark:bg-[#1b4b1f]">
+      <div className="text-center space-y-4">
+        <h1 className="text-4xl font-bold text-[#1b4b1f] dark:text-[#d1f7c4]">
+          Coming Soon
+        </h1>
+        <p className="text-lg text-[#1b4b1f] dark:text-[#d1f7c4]">
+          Connect Internal under construction. Stay tuned for the first
+          iteration of our website.
+        </p>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default LandingPage;
+export default LandingPage
